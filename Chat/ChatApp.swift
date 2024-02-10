06e6cb3871b72f31ffebaff63ab071b7f874a203
drothomas808 <156823052+drothomas808@users.noreply.github.com>
2024-02-10 04:12:36 -0500

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Pedro Thomas on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
